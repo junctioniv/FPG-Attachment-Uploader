4e75347161cc00ecbf289019ae7251c86ade8e7a
James Taylor <junctioniv@gmail.com>
2018-07-02 11:49:21 -0400

    Add project files.

---
+++ FPG Attachment Uploader/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPG_Attachment_Uploader
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
