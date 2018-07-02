13f5bc62d6835ab625286056b8fae7d904e60d38
James Taylor <junctioniv@gmail.com>
2018-07-02 13:42:00 -0400

    Intital Commit. Adding Concur SDK and their ConcurClient class from their examples.

--- FPG Attachment Uploader/Program.cs
+++ FPG Attachment Uploader/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Concur.Authentication;
 
 namespace FPG_Attachment_Uploader
 {
@@ -10,6 +11,12 @@ namespace FPG_Attachment_Uploader
 	{
 		static void Main(string[] args)
 		{
+			var authService = new AuthenticationService();
+			var oauthDetail = authService.GetOAuthTokenAsync(loginId, password, clientId).Result;
+
+			serviceV3 = new Concur.Connect.V3.ConnectService(oauthDetail.AccessToken, oauthDetail.InstanceUrl);
+			serviceV1 = new Concur.Connect.V1.ConnectService(oauthDetail.AccessToken, oauthDetail.InstanceUrl);
+			return oauthDetail;
 		}
 	}
 }
